12eac1e2911cab7bcc190285688fa2676f24ea35
yitenchen123 <2916782430@qq.com>
2026-06-14 19:47:15 +0800

    更新 MinecraftResourceDownloadTask.h

--- Natives/MinecraftResourceDownloadTask.h
+++ Natives/MinecraftResourceDownloadTask.h
@@ -7,8 +7,16 @@
 @property NSMutableArray *fileList, *progressList;
 @property NSMutableDictionary* metadata;
 @property(nonatomic, copy) void(^handleError)(void);
+@property(nonatomic, copy) void(^modpackDownloadCompletion)(void);
+
+// 新增方法声明（用于账户检查）
+- (BOOL)checkAccessWithDialog:(BOOL)show;
+
+- (void)prepareForDownload;
 
 - (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path;
+- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path success:(void (^)())success;
+
 - (void)finishDownloadWithErrorString:(NSString *)error;
 
 - (void)downloadVersion:(NSDictionary *)version;
