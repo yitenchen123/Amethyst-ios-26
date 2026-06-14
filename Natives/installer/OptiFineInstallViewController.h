c419c80082a7fe07f20142b95311e58142500ad3
yitenchen123 <2916782430@qq.com>
2026-06-14 18:26:43 +0800

    创建 OptiFineInstallViewController.h

---
+++ Natives/installer/OptiFineInstallViewController.h
@@ -0,0 +1,11 @@
+//  OptiFineInstallViewController.h
+//  Amethyst
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OptiFineInstallViewController : UITableViewController <UISearchResultsUpdating>
+@end
+
+NS_ASSUME_NONNULL_END
